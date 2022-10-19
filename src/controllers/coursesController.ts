373c02ceeabb2b13f321d09134715c8157f5c9b1
hammer200911 <jdaniel231@gmail.com>
2022-10-19 11:34:19 -0300

    busca curso

--- src/controllers/coursesController.ts
+++ src/controllers/coursesController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { getPaginationParams } from "../helpers/getPaginationParams";
 import { courseService } from "../services/courseService";
 
 export const coursesController = {
@@ -7,6 +8,21 @@ export const coursesController = {
     try {
       const featuredCourses = await courseService.getRandomFeaturedCourses()
       return res.json(featuredCourses)
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message })
+      }
+    }
+  },
+
+  // GET /courses/search
+  search: async (req: Request, res: Response) => {
+    const { name } = req.query
+    const [page, perPage] = getPaginationParams(req.query)
+    try {
+      if(typeof name !== 'string') throw new Error('name param must be of type string')
+        const searchCourses = await courseService.findByName(name, page, perPage)
+        return res.json(searchCourses)
     } catch (err) {
       if (err instanceof Error) {
         return res.status(400).json({ message: err.message })
--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -1,3 +1,4 @@
+import { Op } from "sequelize"
 import { Course } from "../models"
 
 export const courseService = {
@@ -50,5 +51,32 @@ export const courseService = {
     })
 
     return courses
+  },
+
+  findByName: async (name: string, page: number, perPage: number) => {
+    const offset = (page - 1) * perPage
+    
+    const { count, rows } = await Course.findAndCountAll({
+      attributes: [
+        'id', 
+        'name', 
+        'synopsis', 
+        ['thumbnail_url', 'thumbnailUrl']
+      ],
+      where: {
+        name: {
+          [Op.iLike]: `%${name}%`
+        }
+      },
+      limit: 10,
+      offset
+    })
+    return{
+      courses: rows,
+      page: page,
+      perPage: perPage,
+      total: count
+    }
   }
+
 }
