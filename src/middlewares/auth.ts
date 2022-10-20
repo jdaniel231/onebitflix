9f7c079eea1e2dba0f2ba0f5df5473de61f01903
hammer200911 <jdaniel231@gmail.com>
2022-10-20 13:20:59 -0300

    acesso permissao video

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -16,14 +16,35 @@ export function ensureAuth(req: AuthenticatedRequest, res: Response, next: NextF
 
   const token = authorizationHeader.replace(/Bearer /, '')
 
-  jwtService.verifyToken(token, (err, decoded) => {
+  jwtService.verifyToken(token, async(err,  decoded) => {
     if(err || typeof decoded == 'undefined') return res.status(401).json({
       message: 'Não Autorizado: token invalido'
     })
 
-    userService.findByEmail((decoded as JwtPayload).email).then(user => {
+    const user = await userService.findByEmail((decoded as JwtPayload).email)
       req.user = user
       next()
-    })
   })
+}
+
+// PROTEGENDO VIDEO
+export function ensureAuthViaQuery(req: AuthenticatedRequest, res: Response, next: NextFunction) {
+  const { token } = req.query
+
+  if(!token) return res.status(401).json({message: 'Não Autorizado'})
+
+  if(typeof token !== 'string') return res.status(400).json({
+    message: "O parametro token deve ser do tipo string"
+  })
+
+  jwtService.verifyToken(token, async (err, decoded)=> {
+    if(err || typeof decoded == 'undefined') return res.status(401).json({
+      message: 'Não Autorizado: token invalido'
+    })
+
+    const user = await userService.findByEmail((decoded as JwtPayload).email)
+    req.user = user
+    next()
+  })
+  
 }
